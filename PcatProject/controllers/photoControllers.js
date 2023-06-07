d862c1894b4558c7a5cf6ccd3c1f75021c8a4eae
ayseleynavuz <ayseleynavuz@gmail.com>
2023-06-07 15:19:29 +0300

    mvc add

--- PcatProject/controllers/photoControllers.js
+++ PcatProject/controllers/photoControllers.js
@@ -4,11 +4,22 @@ const fs = require('fs');
 
 // get all photos
 exports.getAllPhotos = async (req, res) => {
-    const photos = await Photo.find({}).sort({dateCreated:-1});
-    //const photos = await Photo.find({}).sort('-dateCreated');
-    res.render('index', {
-      photos: photos,
-    });
+  const page = req.query.page || 1; // if there is no page query, set it to 1
+  const photosPerPage = 1; // display 1 photo per page
+  const totalPhotos = await Photo.find().countDocuments(); // count the total number of photos in the database
+
+  const photos = await Photo.find({})
+  .sort('-dateCreated')
+  .skip((page - 1) * photosPerPage) // skip the photos that are not in the current page
+  .limit(photosPerPage); // limit the number of photos per page
+
+
+  res.render('index', {
+    photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage), // round up the number of pages
+  });
+
 }
 
 // get photo by id
@@ -46,13 +57,12 @@ exports.updatePhoto = async (req, res) => {
     photo.description = req.body.description
     photo.save()
     res.redirect(`/photos/${req.params.id}`)
-}
+};
 
 // delete photo
-exports.deletePhoto = async (req, res) => {
-    const photo = await Photo.findOne({_id: req.params.id})
-    let deletedImage = __dirname + '/../public' + photo.image; // to delete the image from the public folder
-    fs.unlinkSync(deletedImage);
-    await Photo.findByIdAndRemove(req.params.id);
+
+exports.deletePhoto  = async (req, res) => {
+    const photo = await Photo.findByIdAndRemove(req.params.id)
+    fs.unlinkSync(__dirname + `/../public/${photo.image}`)
     res.redirect('/')
-}
+} ;
